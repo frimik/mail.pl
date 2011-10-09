df9fb5a645a437ad2e43df65818d4d9153368078
Mikael Fridh <frimik@gmail.com>
2011-10-09 22:13:26 +0200

    v1.0

--- mail.pl
+++ mail.pl
@@ -1,18 +1,6 @@
 #! /usr/bin/env perl
 
-=head1 NAME
-
-mail.pl - Fancier mail sender.
-
-=head1 SYNOPSIS
-
-mail.pl [options] [recipients ...]
-
-  Options:
-    --help    brief help message
-    --man     full documentatin
-
-=cut
+package TMTOWTDI::Mail;
 
 use warnings;
 use strict;
@@ -30,9 +18,102 @@ my $subject;
 my @bcc;
 my @cc;
 my @attachments;
-my $verbose = '';
+my $verbose = 0;
 my $help = 0;
 my $man = 0;
+
+GetOptions(
+  's|subject=s'         => \$subject,
+  'f|from=s'            => \$from,
+  'b|bcc-addr=s{,20}'   => \@bcc,
+  'c|cc-addr=s{,20}'    => \@cc,
+  'a|attach=s{,100}'    => \@attachments,
+  'v|verbose'           => \$verbose,
+  'h|help'              => \$help,
+  'man'                 => \$man,
+) or pod2usage(2);
+
+pod2usage(1) if $help;
+pod2usage({ -verbose => 2 }) if $man;
+
+my $to = join(',', @ARGV);
+
+pod2usage( {
+    -message => "No recipients specified (To:)",
+    -verbose => 1,
+  } ) unless $to;
+
+my %opts = ();
+
+$opts{To} = $to;
+
+if ($subject) {
+  $opts{Subject} = $subject;
+}
+
+if ($from) {
+  $opts{From} = $from;
+}
+
+if (@bcc) {
+  $opts{Bcc} = join(',', @bcc);
+}
+
+if (@cc) {
+  $opts{Cc} = join(',', @cc);
+}
+
+my $data;
+
+{
+  local $/;
+  $data = <STDIN>;
+}
+
+$opts{Data} = $data;
+
+my $msg = MIME::Lite->new(
+  %opts
+);
+
+sub get_encoding
+{
+  my $file = shift;
+
+  my ($mediatype, $encoding) = by_suffix($file);
+  print "$file: $encoding\n" if $verbose;
+  return ($mediatype, $encoding);
+
+}
+
+foreach my $file (@attachments) {
+  if (-f $file) {
+    my ($mediatype, $encoding) = get_encoding($file);
+    $msg->attach(
+      Type      => $mediatype,
+      Encoding  => $encoding,
+      Path      => $file,
+      Filename  => basename($file),
+      Disposition => 'attachment',
+    );
+  }
+}
+
+$msg->send;
+
+1;
+
+=head1 NAME
+
+mail.pl - Fancier mail sender.
+
+=head1 SYNOPSIS
+
+mail.pl [options] [recipients ...]
+
+  Options:
+    --help    brief help message
+    --man     full documentatin
 
 =head1 OPTIONS
 
@@ -72,74 +153,8 @@ Add attachments to the message.
 
 B<mail.pl> will send mail to all recipients passed as arguments.
 
+=head1 AUTHOR
+
+Mikael Fridh <frimik@gmail.com>
+
 =cut
-
-GetOptions(
-  's|subject=s'         => \$subject,
-  'f|from=s'            => \$from,
-  'b|bcc-addr=s{,20}'   => \@bcc,
-  'c|cc-addr=s{,20}'    => \@cc,
-  'a|attach=s{,100}'    => \@attachments,
-  'v|verbose'           => \$verbose,
-  'h|help'              => \$help,
-  'man'                 => \$man,
-) or pod2usage(2);
-
-pod2usage(1) if $help;
-pod2usage({ -verbose => 2 }) if $man;
-
-my $to = join(',', @ARGV);
-
-pod2usage(2) unless $to;
-
-my %opts = ();
-
-if ($subject) {
-  $opts{Subject} = $subject;
-}
-
-if ($from) {
-  $opts{From} = $from;
-}
-
-if (@bcc) {
-  $opts{Bcc} = join(',', @bcc);
-}
-
-if (@cc) {
-  $opts{Cc} = join(',', @cc);
-}
-
-my $data;
-
-{
-  local $/;
-  $data = <STDIN>;
-}
-
-$opts{Data} = $data;
-
-my $msg = MIME::Lite->new(
-  %opts
-);
-
-# Process the attachments
-my $mimetypes = MIME::Types->new;
-
-foreach my $file (@attachments) {
-  if (-f $file) {
-    my ($mediatype, $encoding) = by_suffix($file);
-    print Dumper($mediatype);
-    print Dumper($encoding);
-    $msg->attach(
-      Type      => $mediatype,
-      Encoding  => $encoding,
-      Path      => $file,
-      Filename  => basename($file),
-      Disposition => 'attachment',
-    );
-  }
-}
-
-$msg->print;
-
